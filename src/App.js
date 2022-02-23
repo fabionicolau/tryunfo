29a5472bb84452330872fb0d3e9d1046e2fcf3e0
fabionicolau <fabionicolaujr@gmail.com>
2022-02-23 17:05:54 -0300

    requisito 4

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Card from './components/Card';
 class App extends React.Component {
   constructor() {
     super();
+    this.state = {
+      cards: [],
+    };
     this.onInputChange = this.onInputChange.bind(this);
     // this.onSaveButtonClick = this.onSaveButtonClick.bind(this);
   }
@@ -17,14 +20,41 @@ class App extends React.Component {
     });
   }
 
+  // onSaveButtonClick(event) {
+  //   event.preventDefault();
+  //   this.setState((previous) => ({
+  //     cards: [...previous.cards, newCard],
+  //   }));
+  // }
+
   render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+    } = this.state;
+
     return (
       <div>
         <Form
           onInputChange={ this.onInputChange }
           onSaveButtonClick={ this.onSaveButtonClick }
         />
-        <Card state={ this.state } />
+        <Card
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardImage={ cardImage }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
+        />
       </div>
     );
   }
