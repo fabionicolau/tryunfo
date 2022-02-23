f18edcf21e68aa252406b1e39bcdf3fef4d3e416
fabionicolau <fabionicolaujr@gmail.com>
2022-02-23 18:37:46 -0300

    requisitos 5 e 6

--- src/App.js
+++ src/App.js
@@ -6,10 +6,20 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: '0',
+      cardAttr2: '0',
+      cardAttr3: '0',
+      cardImage: '',
+      cardRare: 'normal',
+      cardTrunfo: false,
+      isSaveButtonDisabled: true,
       cards: [],
     };
     this.onInputChange = this.onInputChange.bind(this);
-    // this.onSaveButtonClick = this.onSaveButtonClick.bind(this);
+    this.onSaveButtonClick = this.onSaveButtonClick.bind(this);
+    this.ValidateForm = this.ValidateForm.bind(this);
   }
 
   onInputChange({ target }) {
@@ -17,17 +27,10 @@ class App extends React.Component {
     const valor = target.type === 'checkbox' ? checked : value;
     this.setState({
       [name]: valor,
-    });
+    }, () => this.ValidateForm());
   }
 
-  // onSaveButtonClick(event) {
-  //   event.preventDefault();
-  //   this.setState((previous) => ({
-  //     cards: [...previous.cards, newCard],
-  //   }));
-  // }
-
-  render() {
+  onSaveButtonClick(event) {
     const {
       cardName,
       cardDescription,
@@ -36,24 +39,80 @@ class App extends React.Component {
       cardAttr3,
       cardImage,
       cardRare,
+      cardTrunfo } = this.state;
+
+    const newCard = {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
       cardTrunfo,
+    };
+    event.preventDefault();
+    this.setState((previous) => ({
+      cards: [...previous.cards, newCard],
+    }));
+    this.setState(() => ({
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: '0',
+      cardAttr2: '0',
+      cardAttr3: '0',
+      cardImage: '',
+      cardRare: 'normal',
+      cardTrunfo: false,
+      isSaveButtonDisabled: true,
+    }));
+  }
+
+  ValidateForm() {
+    const {
+      cardName,
+      cardDescription,
+      cardImage, cardRare, cardAttr1, cardAttr2,
+      cardAttr3,
+    } = this.state;
+
+    const limitCard = 90;
+    const sum = 210;
+    const atributo1 = parseInt(cardAttr1, 10);
+    const atributo2 = parseInt(cardAttr2, 10);
+    const atributo3 = parseInt(cardAttr3, 10);
+
+    if (cardName === ''
+      || cardDescription === '' || cardImage === '' || cardRare === ''
+      || atributo1 > limitCard || atributo2 > limitCard || atributo3 > limitCard
+      || atributo1 + atributo2 + atributo3 > sum
+      || atributo1 < 0 || atributo2 < 0 || atributo3 < 0
+    ) {
+      this.setState({
+        isSaveButtonDisabled: true,
+      });
+    } else {
+      this.setState({
+        isSaveButtonDisabled: false,
+      });
+    }
+  }
+
+  render() {
+    const {
+      isSaveButtonDisabled,
     } = this.state;
 
     return (
       <div>
         <Form
+          { ...this.state }
           onInputChange={ this.onInputChange }
           onSaveButtonClick={ this.onSaveButtonClick }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
         />
         <Card
-          cardName={ cardName }
-          cardDescription={ cardDescription }
-          cardAttr1={ cardAttr1 }
-          cardAttr2={ cardAttr2 }
-          cardAttr3={ cardAttr3 }
-          cardImage={ cardImage }
-          cardRare={ cardRare }
-          cardTrunfo={ cardTrunfo }
+          { ...this.state }
         />
       </div>
     );
